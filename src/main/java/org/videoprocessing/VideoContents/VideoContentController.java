4f649a902681c668ac19a8721283680d3d6f626e
arcyck <94861028+arcyck@users.noreply.github.com>
2026-07-23 15:01:10 -0700

    Added swagger docs

--- src/main/java/org/videoprocessing/VideoContents/VideoContentController.java
+++ src/main/java/org/videoprocessing/VideoContents/VideoContentController.java
@@ -1,6 +1,8 @@
 package org.videoprocessing.VideoContents;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,7 +23,8 @@ public class VideoContentController {
         this.videoContentsService = videoContentsService;
     }
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Tag(name = "Upload", description = "Uploads a file")
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
             VideoContents videoContents = videoContentsService.uploadFile(file);
@@ -33,6 +36,7 @@ public class VideoContentController {
     }
 
     @GetMapping("/{id}/thumbnail")
+    @Tag(name = "Thumbnail", description = "Gets the thumbnail of the video. Hardcoded at 1 second of the video.")
     public ResponseEntity<?> getVideoThumbnail(@PathVariable String id) {
         String imageUrl = videoContentsService.getVideoThumbnail(UUID.fromString(id));
         Map<String, String> map = new HashMap<>();
@@ -41,6 +45,7 @@ public class VideoContentController {
     }
 
     @PostMapping("/{id}/change-scale")
+    @Tag(name = "Video scale", description = "Changes the video scale by Id")
     public ResponseEntity<?> adjustVideoScale(@PathVariable String id, @Valid @RequestBody VideoScaleDTO videoScaleDTO) {
         String videoUrl = videoContentsService.changeVideoResolution(UUID.fromString(id),
                 videoScaleDTO.height(), videoScaleDTO.width());
@@ -50,6 +55,7 @@ public class VideoContentController {
     }
 
     @GetMapping("/all")
+    @Tag(name = "All", description = "Gets all of the uploaded videos")
     public ResponseEntity <List<VideoContents>> getAllVideos() {
         return ResponseEntity.ok(videoContentsService.getAllVideos());
     }
